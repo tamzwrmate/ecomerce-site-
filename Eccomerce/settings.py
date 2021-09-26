1122980f9ecc4f1c0c835220430382944e06d9ae
tamzwrmate <87138625+tamzwrmate@users.noreply.github.com>
2021-09-26 17:45:44 +0200

    Update settings.py

--- Eccomerce/settings.py
+++ Eccomerce/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
